6183ce5428e9aa30e58c133d287569744e4e1499
Burak Can KARAKURT <84005554+jellybeanist@users.noreply.github.com>
2021-05-20 15:50:49 +0300

    Update uart_rx.vhd
    commands are deleted. clean code improvements.

--- uart_rx.vhd
+++ uart_rx.vhd
@@ -5,25 +5,23 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity uart_rx is
 generic (
-        CLK_FREQ        : INTEGER := 100_000_000;   --100 MHz clock.
-        BAUD_RATE       : INTEGER := 115_200        --Baudrate.
+        CLK_FREQ        : INTEGER := 100_000_000;  
+        BAUD_RATE       : INTEGER := 115_200     
 );
 port (
-        CLK             : IN STD_LOGIC;                         --1 bit clock.  
-        RST_IN          : IN STD_LOGIC;                         --1 bit reset.      
-        RX_IN           : IN STD_LOGIC;                         --1 bit input.
-        RX_OUT          : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);    --Data to transmit after receiving.        
-        RX_DONE_OUT     : OUT STD_LOGIC                         --Done check.
-
-
+        CLK             : IN STD_LOGIC;                         
+        RST_IN          : IN STD_LOGIC;                            
+        RX_IN           : IN STD_LOGIC;                       
+        RX_OUT          : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);        
+        RX_DONE_OUT     : OUT STD_LOGIC   
 );
 end uart_rx;
 
 architecture Behavioral of uart_rx is
 
 type states is (IDLE, START, RECEIVE, STOP);
-signal state : states := IDLE; --IDLE is the default state.
-constant clock_count_lim : integer := CLK_FREQ/BAUD_RATE+1;  --How many clocks to wait for bit check
+signal state : states := IDLE; 
+constant clock_count_lim : integer := CLK_FREQ/BAUD_RATE+1; 
 
 
 signal clock_counter : integer range 0 to (clock_count_lim)-1 := 0;
@@ -33,10 +31,10 @@ signal shift_start_receive : std_logic_vector(2 downto 0) := (others=>'0');
 signal rx_done : std_logic := '0';
 
 begin
-    RX_OUT      <= data;
-    RX_DONE_OUT <= rx_done;
+    RX_OUT      <=  data;
+    RX_DONE_OUT <=  rx_done;
 
-MAIN :  process (CLK) begin
+MAIN :  process (CLK,RST_IN) begin
         
             if (RST_IN = '1') then
                 state <= IDLE;
@@ -51,14 +49,11 @@ MAIN :  process (CLK) begin
                 rx_done <= '0';
                     case state is
                         when IDLE =>
-                            --default parameters, program stands here.
-                            --stays here until the change of high to low, which means start receiving.
                             if (shift_start_receive(2 downto 1) = "10") then
                                 state	<= START;
                             end if;
                     
                         when START =>
-                        -- tx_out is equal to 0 when program enters here.
                             if (clock_counter = (clock_count_lim-1)/2) then
                                 state           <= RECEIVE; 
                                 clock_counter	<= 0;
